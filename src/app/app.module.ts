e9bcd8b72fbe6aa461e574db63820b85e0c8e930
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-25 16:37:31 +0700

    add some components and update bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaglineComponent } from './tagline/tagline.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaglineComponent,
+    PostsComponent,
+    PostListComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
